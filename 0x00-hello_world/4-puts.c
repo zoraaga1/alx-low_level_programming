47032d942e91f4f274ad2201acff66eeda70b257
zoraaga1 <fati.sousse12@gmail.com>
2023-06-15 15:37:53 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /*
  *the Hello code
+ *main -the code
  */
 int main(void)
 {
 char Hello[] = "\"Programming is like building a multilingual puzzle";
-puts(Hello);
+puts (Hello);
 return(0);
 }
