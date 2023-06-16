63c4bb5bafd2f40485d6ed8ad41f9d473902f16a
zoraaga1 <fati.sousse12@gmail.com>
2023-06-16 16:54:36 -0700

    Print alphabets in lowercase and uppercase except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter;
+
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	if (letter = q || letter = e)
+	{
+		break;
+	}
+	{
+		putchar(letter);
+	}
+	
+	putchar('\n');
+	return (0);
+}
