76d7e4b9446ca5102d4ec6ab94505bdd6d938e94
zoraaga1 <fati.sousse12@gmail.com>
2023-06-15 17:53:17 -0700

    Dont use printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - print a string
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	write(1,"and that piece of art is useful\" Dora Korpar, 2015-10-19",strlen("1,"and that piece of art is useful\" Dora Korpar, 2015-10-19'));
+	return (0);
