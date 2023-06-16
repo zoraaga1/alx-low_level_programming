597d0851ed52f50cb77350f677d33721c35313d6
zoraaga1 <fati.sousse12@gmail.com>
2023-06-15 18:01:51 -0700

    Dont use printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
 */
 int main(void)
 {
-        const char msg[] = "and that piece of art is useful" -  Dora Korpar, 2015-10-19"; 
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
+        write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
