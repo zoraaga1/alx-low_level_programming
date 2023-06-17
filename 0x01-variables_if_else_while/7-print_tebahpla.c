65c8e32071818f88a9963ab19a443f089dde323e
zoraaga1 <fati.sousse12@gmail.com>
2023-06-17 06:11:24 -0700

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print reverse alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar (letter);
+	}
+	return (0);
+}
