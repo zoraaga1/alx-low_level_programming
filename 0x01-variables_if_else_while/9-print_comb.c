bd649ec9c68d3e4f3acdce572330a5ea76f7953a
zoraaga1 <fati.sousse12@gmail.com>
2023-06-17 08:01:10 -0700

    Print numbers with coma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,9 @@ int main(void)
 	{
 		putchar (a + '0');
 		if (a < 9)
-		{putchar (',');}
+		{putchar (','); }
 		if (a < 9)
-		{putchar (' ');}
+		{putchar (' '); }
 	}
 	putchar ('\n');
 	return (0);
