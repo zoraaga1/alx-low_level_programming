f838cea3eaaf3645a3ee1d25ebf5715e522fb868
zoraaga1 <fati.sousse12@gmail.com>
2023-06-17 06:02:02 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print int
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a< 10; a++)
+	{
+		putchar (a + '0');
+	}
+	return (0);
+}
