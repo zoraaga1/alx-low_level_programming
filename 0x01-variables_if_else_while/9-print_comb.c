f390293bb14faf5def8ead2c44b52c450e9af67d
zoraaga1 <fati.sousse12@gmail.com>
2023-06-17 07:17:38 -0700

    Print numbers with coma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print numbers with coma
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 0; a < 10; ++a)
+	{
+		putchar (a + '0');
+		if (a < 9)
+		{putchar ('\,');}
+		if (a < 9)
+		{putchar (' ');}
+	}
+}
