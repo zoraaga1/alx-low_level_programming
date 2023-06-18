c3a3cd762aa6557978c08e40d5185323000f787d
zoraaga1 <fati.sousse12@gmail.com>
2023-06-18 06:36:43 -0700

    From 01 to 89)

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,72 +10,18 @@ int main(void)
 	int b;
 
 	for (a = 0; a < 9; a++)
-	{for (b = 1; b < 10; b++)
-		{
-			if (a == b)
-			{
-				continue;
-			}
-			if (a == 2)
-			{
-				if (b < 2)
-				{
-					continue;
-				}
-			}
-			if (a == 3)
-			{
-				if (b < 4)
-				{
-					continue;
-				}
-			}
-			if (a == 4)
-			{
-				if (b < 4)
-				{
-					continue;
-				}
-			}
-			if (a == 5)
-			{
-				if (b < 6)
-				{
-					continue;
-				}
-			}
-			if (a == 6)
-			{
-				if (b < 7)
-				{
-					continue;
-				}
-			}
-			if (a == 7)
-			{
-				if (b < 8)
-				{
-					continue;
-				}
-			}
-			if (a == 8)
-			{
-				if (b < 9)
-				{
-					continue;
-				}
-			}       
-	putchar (a + '0');
-	putchar (b + '0');
-	if (a == 8)
 	{
-		if (b == 9)
-		continue;
-	}
-	putchar (',');
-	putchar (' ');
+		for (b = 1; b < 10; b++)
+		{
+			putchar(a % 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar ('\n');
-	return 0;        
+	putchar('\n');
+	return (0);
 }
+		
