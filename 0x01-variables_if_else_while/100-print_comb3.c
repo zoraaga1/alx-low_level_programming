d1d3c4166cb698c1a7a545fa759af207ebbc9f07
zoraaga1 <fati.sousse12@gmail.com>
2023-06-17 17:26:34 -0700

    From 01 t o 98

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,80 +1,83 @@
-
 #include <stdio.h>
+/**
+ * main - print combination of digit from 1 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a; 
+	int b;
 
-int main(void) {
-  int a; 
-  int b;
-
-  for (a = 0; a < 9; a++){
-    for (b = 1; b < 10; b++){
-        
-       if (a == b){
-           
-       continue;
-       }
-       if (a == 2){
-           if (b < 2){
-           continue;
-           }
-       }
+	for (a = 0; a < 9; a++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+			if (a == b)
+			{
+				continue;
+			}
+			if (a == 2)
+			{
+				if (b < 2)
+				{
+					continue;
+				}
+			}
+			if (a == 3)
+			{
+				if (b < 4)
+				{
+					continue;
+				}   
+			}
+			if (a == 4)
+			{
+				if (b < 4)
+				{
+					continue;
+				}
+			}
        
-       if (a == 3){
-           if (b < 4){
-           continue;
-           }   
-       }
-       
-       if (a == 4){
-           if (b < 5){
-           continue;
-           }   
-       }
-       
-         if (a == 5){
-           if (b < 6){
-           continue;
-           }   
-       }
-       
-         if (a == 6){
-           if (b < 7){
-           continue;
-           }   
-       }
-       
-         if (a == 7){
-           if (b < 8){
-           continue;
-           }   
-       }
-       
-         if (a == 8){
-           if (b < 9){
-           continue;
-           }   
-       }
-
-       
-       putchar (a + '0');
-       putchar (b + '0');
-       if (a == 8){
-           if (b == 9)
-           continue;
-       }
-       putchar (',');
-       putchar (' ');
-      
-       
-       }
-       
-      
-    }
-   
-
-	
-    return 0;
-    
-  
-
-        
+			if (a == 5)
+			{
+				if (b < 6)
+				{
+					continue;
+				}
+			}
+			if (a == 6)
+			{
+				if (b < 7)
+				{
+					continue;
+				}
+			}
+			if (a == 7)
+			{
+				if (b < 8)
+				{
+					continue;
+				}
+			}
+			if (a == 8)
+			{
+				if (b < 9)
+				{
+					continue;
+				}
+			}       
+	putchar (a + '0');
+	putchar (b + '0');
+	if (a == 8)
+	{
+		if (b == 9)
+		continue;
+	}
+	putchar (',');
+	putchar (' ');
+		}
+	}
+	putchar ('\n');
+	return 0;        
 }
