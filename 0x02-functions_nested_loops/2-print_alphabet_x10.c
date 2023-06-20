3239bc420ef9ec0d20783d68fdf189d13fcfcf7e
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 13:42:44 -0700

    void print_alphabet_x10(void);

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10 - print 10 times the alphabet a-z in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i = 0;
+
+	do
+	{
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		putchar(letter);
+	}
+	i++;
+	putchar('\n');
+	}
+	while (i != 10);
+}
