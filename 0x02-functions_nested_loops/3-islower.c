b42d9d6c181a6743ad8b89d689abc33528475cec
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 15:29:02 -0700

    Check if the input is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _islower - prints 
+ *
+ * Return: 1 if c is lowercase
+ * return: 0 if c in not in lowercase 
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
