d2ee5bc85ac12b153355a33f72fe1bf1d59dcc27
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 13:43:53 -0700

    void print_alphabet_x10(void);

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,10 @@ void print_alphabet_x10(void)
 	{
 	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
-		putchar(letter);
+		_putchar(letter);
 	}
 	i++;
-	putchar('\n');
+	_putchar('\n');
 	}
 	while (i != 10);
 }
