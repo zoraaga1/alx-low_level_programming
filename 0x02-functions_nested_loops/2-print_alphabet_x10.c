c47e9104f31af8974370c9fb83adc1254ca25e3b
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 13:49:57 -0700

    void print_alphabet_x10(void);

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 	char letter;
 	int i = 0;
 
-	do{
+	do	{
 	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
 		_putchar(letter);
 	}
 	i++;
 	_putchar('\n');
-	}while (i != 10);
+	}	while (i != 10);
 }
