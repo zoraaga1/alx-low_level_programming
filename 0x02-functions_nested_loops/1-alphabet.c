202bc55aed21285a76d98dfa5ccd08fd6a865f7d
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 13:22:26 -0700

    void print_alphabet(void);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - print alphabet a-z in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
