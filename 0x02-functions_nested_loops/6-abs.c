297e5befcea7993b3d05cc8171413d117b171c93
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 17:30:32 -0700

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _abs - prints the absolute value of an integer
+ *n : int type integer
  *
  * Return: 0 (Success)
  */
 
 int _abs(int n)
 {
-	if (n >=0)
+	if (n >= 0)
 	{
 		return (n);
 	}
