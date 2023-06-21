44e324352df0fbfb8aa99724d615264a8a19d9a7
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 17:56:50 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,25 @@
 #include "main.h"
 /**
  * print_last_digit - print 10 times the alphabet a-z in lowercase
- * @m: int type integer 
+ * @n: int type integer
  *
  * Return: 0 (Success)
  */
 
-int print_last_digit(int m)
+int print_last_digit(int n)
 {
 	int lastdigit;
-	
-	m = _abs(m);
-	lastdigit = m % 10;
-	return (lastdigit);
+
+	if (n < 0)
+	{
+		nv = -1 * (n % 10);
+		_putchar(lastdigit + '0');
+		return (lastdigit);
+	}
+	else
+	{
+		lastdigit = n % 10;
+		_putchar(lastdigit + '0');
+		return (lastdigit);
+	}
 }
