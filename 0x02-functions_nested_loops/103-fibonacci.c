7596ade93fdb720d4d913f94a77d15d67158d0ce
zoraaga1 <fati.sousse12@gmail.com>
2023-06-21 16:58:15 -0700

    Print the sum of even Fibonacci numbers up to a fib value

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of fibonacci numbers
+ * not exceeding 4,000,000.
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 1, b = 2;
+	int sum = 0;
+	int c;
+
+	while (b < 4000000)
+	{
+		if (b % 2 == 0)
+			total += b;
+
+		c = b;
+		b += i;
+		a = c;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
