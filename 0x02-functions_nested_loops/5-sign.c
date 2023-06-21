0fe5f681b6768a9349e556e1aafacdcd33a7e67e
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 17:04:05 -0700

    Number sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,9 @@
  * "0" for zero number
  *@n : int type number
  *
- * Return: 1 if the number is greater than 0, -1 if the number is less than 0, 0 if the number is zero
+ * Return: 1 if the number is greater than 0,
+ *	-1 if the number is less than 0, 
+ * 	0 if the number is zero
  */
 
 int print_sign(int n)
