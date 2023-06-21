760cede98b99fe97ac344a21669059d752ac3bdd
zoraaga1 <fati.sousse12@gmail.com>
2023-06-20 17:23:05 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ *
+ * Return: 0 (Success)
+ */
+
+int _abs(int)
+{
+	if (int >=0)
+	{
+		_putchar('int');
+	}
+	else
+	{
+		_putchar('-');
+	}
+	return (0);
+}
