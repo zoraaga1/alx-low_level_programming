7c398827a4e6088e1cac22d98eebf7c8ee558de6
zoraaga1 <fati.sousse12@gmail.com>
2023-06-22 10:57:43 -0700

    Is alpha uppercase or not ?

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - Return 1 to the main if the char c is uppercase alphabet
+ *              Return 0 to the main if the char is not uppercase alphabet
+ * @c : char a-z or A-Z
+ *
+ * Return: 1 (Success)
+ *          0 (if c is not uppercase)
+ */
+ 
+int _isupper(int c)
+{
+    if (c > 96 && c < 123)
+    return (0);
+    else
+    return (1);
+}
