848653ecac635da2b29ae59c746fab7c96435ad2
zoraaga1 <fati.sousse12@gmail.com>
2023-06-22 17:16:38 -0700

    Print _ n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - Print the sign _ n time
+ * @n : number of times to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (n <= 1)
+		{
+			putchar('\n');
+		}
+	putchar('_');
+	}
+	putchar('\n');
+}
