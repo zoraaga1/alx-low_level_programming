30982b27c0ec6e34bdc00a96f14ca70e0fc3d4d0
zoraaga1 <fati.sousse12@gmail.com>
2023-06-23 05:08:06 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,12 @@ int main(void)
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
 		else
 			printf("%d ", i);
 	}
