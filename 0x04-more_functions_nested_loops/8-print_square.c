345f1c8af43f6232c2c52647b0ad677c4b54ab11
zoraaga1 <fati.sousse12@gmail.com>
2023-06-22 18:43:17 -0700

    Print square n time

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_square - Print a square for n time
+ * @n : number of times to print
+ */
+void print_square(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - 1; j++)
+		{
+			putchar('#');
+		}
+		putchar('#');
+		putchar('\n');
+	}
+	putchar('\n');
+}
+
