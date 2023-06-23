fdd7ea054de6e83592041dbdf3d3d1c0b7055052
zoraaga1 <fati.sousse12@gmail.com>
2023-06-22 18:26:14 -0700

    Print diagonal n time

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,11 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i <= n - 1; i++)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n - 1; i++)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
