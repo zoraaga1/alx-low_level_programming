21d0bf1cf9fa6deeebc53d226b3518f8ad243283
zoraaga1 <fati.sousse12@gmail.com>
2023-06-23 07:26:11 -0700

    Print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_triangle - Prints triangle with hashkey
+ *@size : size of triangle
+ */
+void print_triangle(int size)
+{
+int times, s, h;
+
+for (times = size; times > 0; times--)
+{
+	for (s = 1 ; s < times; s++)
+	{	_putchar(' ');
+	}
+	for (h = size; h >= times; h--)
+	{
+		_putchar('#');
+	}
+	_putchar('\n');
+}
+
+if (size <= 0)
+	_putchar('\n');
+}
