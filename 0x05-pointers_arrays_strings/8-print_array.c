54d60480aa33b6e14b64a94b0917530430a9f2a7
zoraaga1 <fati.sousse12@gmail.com>
2023-06-26 16:21:46 -0700

    Prints array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - prints array list with comma
+ * @a : array name
+ * @n : array length
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i <= n - 1)
+	{
+		printf("%d", a[i]);
+		if (i == n - 1)
+		{
+			break;
+		}
+		printf(", ");
+		i++;
+	}
+	_putchar('\n');
+}
