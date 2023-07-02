ee5bc782be93a912da77689b63000d8f5e05d392
zoraaga1 <fati.sousse12@gmail.com>
2023-07-01 18:04:00 -0700

    Capitalize all

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++;
+			index++;
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
@@ -28,7 +28,7 @@ char *cap_string(char *str)
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
-			index++;
+		index++;
 	}
 	return (str);
 }
