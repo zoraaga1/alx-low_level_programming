cb352d040bc9aed63ec5112d2e8fdfa26b81e32a
zoraaga1 <fati.sousse12@gmail.com>
2023-07-03 17:31:48 -0700

    Locate substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strstr - locates a substring
+* @haystack: first string
+* @needle: string to compare with
+* Return: Always 0 (Success)
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *j = needle;
+		while (*i == *j && *j != '\0')
+			{
+				i++;
+				j++;
+			}
+		if (*j == '\0')
+			return (haystack);
+	}
+	return (0);
+}
