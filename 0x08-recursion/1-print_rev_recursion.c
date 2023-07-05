07f8ac1286b98cd8d3ca7bab71cceffa3b7f9bad
zoraaga1 <fati.sousse12@gmail.com>
2023-07-05 09:43:10 -0700

    Reverse a string with recursion method

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
    if (*s)
   {
