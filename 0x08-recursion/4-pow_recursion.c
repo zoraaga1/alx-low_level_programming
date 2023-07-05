b66fbc730991a55d76fd774e18e876da1f92a3d7
zoraaga1 <fati.sousse12@gmail.com>
2023-07-05 12:11:52 -0700

    Count the number x in power of y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y
+ * @x: the value of number
+ * @y: the power
+ * Return: 1 for y = 0
+ * -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * (_pow_recursion(x, y - 1)));
+}
