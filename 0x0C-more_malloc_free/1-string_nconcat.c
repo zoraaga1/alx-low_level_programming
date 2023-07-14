a1089464eb329b56855e80d7727f481f48826688
zoraaga1 <fati.sousse12@gmail.com>
2023-07-14 11:22:01 -0700

    Allocate memory for an array, using malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,28 +1,90 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+		
+#include "main.h"
+		
 
+		
 /**
- * create_array - Create an array
- *@size: size of the array
- *@c: character
- * Return: a pointer to the array, or NULL if it fails
+		
+ * *string_nconcat - concatenates n bytes of a string to another string
+		
+ * @s1: string to append to
+		
+ * @s2: string to concatenate from
+		
+ * @n: number of bytes from s2 to concatenate to s1
+		
+ *
+		
+ * Return: pointer to the resulting string
+		
  */
-char *create_array(unsigned int size, char c)
+		
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+		
 {
-	char *arr = NULL;
-	unsigned int i;
+		
+	char *s;
+		
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+		
 
-	if (size == 0)
+		
+	while (s1 && s1[len1])
+		
+		len1++;
+		
+	while (s2 && s2[len2])
+		
+		len2++;
+		
+
+		
+	if (n < len2)
+		
+		s = malloc(sizeof(char) * (len1 + n + 1));
+		
+	else
+		
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+		
+
+		
+	if (!s)
+		
 		return (NULL);
-	if (size != 0)
+		
+
+		
+	while (i < len1)
+		
 	{
-		arr = (char *)malloc(size * sizeof(char));
-		if (arr != NULL)
-		{
-			for (i = 0; i < size; i++)
-				arr[i] = c;
-		}
+		
+		s[i] = s1[i];
+		
+		i++;
+		
 	}
-	return (arr);
+		
+
+		
+	while (n < len2 && i < (len1 + n))
+		
+		s[i++] = s2[j++];
+		
+
+		
+	while (n >= len2 && i < (len1 + len2))
+		
+		s[i++] = s2[j++];
+		
+
+		
+	s[i] = '\0';
+		
+
+		
+	return (s);
+		
 }
+
