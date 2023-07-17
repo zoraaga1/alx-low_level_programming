7464318008520f73bdc743ef890c81ca8a515f0e
zoraaga1 <fati.sousse12@gmail.com>
2023-07-17 09:05:37 -0700

    Print the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ *	followed by a new line 
+ *
+ * Return: Success
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
