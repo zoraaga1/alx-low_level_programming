d0a2b8d56e3492f996dd70d867666e679b6e7cf2
zoraaga1 <fati.sousse12@gmail.com>
2023-07-19 16:25:49 -0700

    Print a string

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef function_pointers_h
 #define function_pointers_h
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
