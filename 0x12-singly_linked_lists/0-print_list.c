ff4855e43a92e61a393e121b5ee8e5ff42352f2a
zoraaga <fati.sousse12@gmail.com>
2023-07-27 16:22:05 -0700

    fix betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/**
+i/**
  *print_list - prints all the elements
 				of a singly linked lists
  *@h :  represents a pointer to the head of a singly linked list
@@ -11,16 +11,14 @@ size_t print_list(const list_t *h)
 {
 	size_t node_count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-
-        h = h->next;
-        node_count++;
-    }
-
-    return node_count;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node_count++;
+	}
+	return node_count;
 }
