b96d6f3a6827108b684939a170f6e5a541eed524
zoraaga <fati.sousse12@gmail.com>
2023-07-27 17:25:32 -0700

    fix errors

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,31 @@
 section .data
-	hello_msg db "Hello, Holberton", 0
-	fmt db "%s", 10, 0   ; Format string for printf: %s for string and 10 for newline (ASCII code)
+    hello_msg db "Hello, Holberton", 10   ; Newline added at the end
 
 section .text
 	global main
-	extern printf
 
 main:
-	push rbp
-	mov rdi, fmt         ; First argument: format string
-	mov rsi, hello_msg   ; Second argument: address of hello_msg string
-	xor rax, rax         ; Clear RAX (return value for printf)
-	call printf
-	pop rbp
-	ret
+	; File descriptor for standard output (1)
+	mov rdi, 1
+
+	; Pointer to the hello_msg string
+	mov rsi, hello_msg
+
+	; Length of the hello_msg string
+	mov rdx, hello_msg_len
+
+	; System call number for write (1)
+	mov rax, 1
+
+	; Invoke the write system call
+	syscall
+
+	; Exit the program
+	xor rdi, rdi   ; Exit code 0
+	mov rax, 60    ; System call number for exit (60)
+	syscall
+
+section .bss
+	; Reserve space for a 64-bit variable to store hello_msg length
+	hello_msg_len resq 1
+
