31d68511454ba3c0585987d3bbe6e797a7c9f3fc
zoraaga <fati.sousse12@gmail.com>
2023-07-27 17:18:26 -0700

    Write a 64-bit program in assembly that prints Hello, Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,16 @@
 section .data
-    hello_msg db "Hello, Holberton", 0
-    fmt db "%s", 10, 0   ; Format string for printf: %s for string and 10 for newline (ASCII code)
+	hello_msg db "Hello, Holberton", 0
+	fmt db "%s", 10, 0   ; Format string for printf: %s for string and 10 for newline (ASCII code)
 
 section .text
-    global main
-    extern printf
+	global main
+	extern printf
 
 main:
-    push rbp
-    mov rdi, fmt         ; First argument: format string
-    mov rsi, hello_msg   ; Second argument: address of hello_msg string
-    xor rax, rax         ; Clear RAX (return value for printf)
-    call printf
-    pop rbp
-    ret
-
+	push rbp
+	mov rdi, fmt         ; First argument: format string
+	mov rsi, hello_msg   ; Second argument: address of hello_msg string
+	xor rax, rax         ; Clear RAX (return value for printf)
+	call printf
+	pop rbp
+	ret
