12ce45f5119b8fe760b98fce8e8545f6df46e971
zoraaga <fati.sousse12@gmail.com>
2023-07-28 17:42:50 -0700

    Fix last task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,19 @@
-section .data
-	hello_msg db "Hello, Holberton", 10   ; Newline added at the end
-	fmt db "%s", 0   ; Format string for printf
+    section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-section .text
-	global main
-	extern printf
+    section .text
+    global main
+    extern printf
 
 main:
-	; Clear the floating-point status word to avoid printf issues on some systems
-	finit
+    push rbp
+    mov rbp, rsp
 
-	; Pass the format string to printf
-	mov rdi, fmt
+    mov rsi, hello
+    mov rdi, format
+    call printf
 
-	; Pass the address of hello_msg string to printf
-	mov rsi, hello_msg
-
-	; Call printf to print the message
-	xor rax, rax   ; Clear RAX (return value for printf)
-	call printf
-
-	; Exit the program
-	xor rdi, rdi   ; Exit code 0
-	mov rax, 60    ; System call number for exit (60)
-	syscall
-
+    pop rbp
+    mov rax, 0
+    ret
