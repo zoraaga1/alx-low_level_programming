908dcec285927b0a1c34611f8239de43c50fb77f
zoraaga <fati.sousse12@gmail.com>
2023-07-31 16:59:51 -0700

    List all elements of a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ *print_listint - prints all the elements
+ *				of a singly linked lists
+ *@h : represents a pointer to the head of a singly linked list
+ *
+ *Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node_count++;
+	}
+	return (node_count);
+}
