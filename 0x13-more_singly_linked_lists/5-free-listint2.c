52d0fd799479beb26d26ed883d19b441db8e7549
zoraaga <fati.sousse12@gmail.com>
2023-08-01 16:26:04 -0700

    set head to null

---
+++ 0x13-more_singly_linked_lists/5-free-listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint -  frees a list_t list.
+ * @head: a pointer to pointer listint_t
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	if (head == NULL || *head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		current= *head;
+		*head = (*(head))->next;
+		free(current);
+	}
+	*head = NULL;
+}
