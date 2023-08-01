49ced83d2ab1979eda88b7487c4f2e35bfb08734
zoraaga <fati.sousse12@gmail.com>
2023-07-31 17:33:24 -0700

    Add a new node to the header

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,23 +3,18 @@
 /**
  * add_nodeint - adds a new node at the beginning of a list_t list
  * @head: a pointer to a pointer of type list_t
- * @n: a pointer to a constant integer
+ * @n: a constant integer
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_nodeint(listint_t **head, const int *n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	list_t *new_node;
+	listint_t *new_node;
 
-	new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = strdup(n);
-	if (new_node->n == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
