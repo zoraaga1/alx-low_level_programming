6c1d9107d1fd3407b0d427f10c08ea3eece277df
zoraaga <fati.sousse12@gmail.com>
2023-08-03 15:53:22 -0700

    get number of bits to fip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - Returns the number of bits needed to flip
+ *		to get from one number to another.
+ * @n: The first number.
+ * @m: The second number.
+ *
+ * Return: The number of bits needed to flip.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor_result;
+	unsigned int count;
+
+	count = 0;
+	xor_result = n ^ m;
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (count);
+}
+
