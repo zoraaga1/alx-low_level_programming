54909ead2c002d68172c4e98ac714b366e4a3231
zoraaga <fati.sousse12@gmail.com>
2023-08-03 15:58:19 -0700

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int mask;
 	int bit_value;
 
-	if (index >= sizeof(unsigned long int) * 8) // Check if the index is out of bounds
+	if (index >= sizeof(unsigned long int) * 8)
         	return (-1);
 	mask = 1UL << index;
 	bit_value = (n & mask) != 0;
