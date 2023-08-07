53f7f524c606ed7789f2bd82d077043e7c609484
zoraaga <fati.sousse12@gmail.com>
2023-08-07 16:51:57 -0700

    Read text file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* Prototypes of used functions */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
