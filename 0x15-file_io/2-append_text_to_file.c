1f434ad7dc32aa7407b64693b180f85c90099343
zoraaga <fati.sousse12@gmail.com>
2023-08-07 17:20:36 -0700

    Append text to the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  * Return: 1 (Success)
  *		-1 (Failure)
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t text_len;
