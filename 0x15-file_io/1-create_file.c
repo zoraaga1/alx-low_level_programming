8b822d326d17e9978568eb04f28203b702b552df
zoraaga <fati.sousse12@gmail.com>
2023-08-07 17:10:59 -0700

    Creat a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-  
+
 	if (text_content != NULL)
 	{
 		text_len = strlen(text_content);
