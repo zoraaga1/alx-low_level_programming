11915b975c4e22f49ffb986ca58c10f3821e20e7
zoraaga1 <133988175+zoraaga1@users.noreply.github.com>
2023-08-31 23:29:29 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,37 +6,35 @@
 
 void print_binary(unsigned long int n)
 {
-	  signed long int _size;
-	  char _c;
-	  int _flag;
-
-	  _size = sizeof(n) * 8 - 1;
-
-	  if (n == 0)
-	    {
-	      printf("0");
-	      return;
-	    }
-
-	  if (n == 1)
-	    {
-	      printf("1");
-	      return;
-	    }
-	  _flag = 0;
-	  while (_size >= 0)
-	    {
-		    _c = (n >> _size) & 1;
-		    if (_flag == 1)
-			    putchar(_c + '0');
-		    else
-		    {
-			    if (_c == 1)
-			    {
-				    putchar(_c + '0');
-				    _flag = 1;
-			    }
-		    }
-		    _size -= 1;
-	    }
+	signed long int _size;
+	char _c;
+	int _flag;
+	
+	_size = sizeof(n) * 8 - 1;
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	if (n == 1)
+	{
+		printf("1");
+		return;
+	}
+	_flag = 0;
+	while (_size >= 0)
+	{
+		_c = (n >> _size) & 1;
+		if (_flag == 1)
+			putchar(_c + '0');
+		else
+		{
+			if (_c == 1)
+			{
+				putchar(_c + '0');
+				_flag = 1;
+			}
+		}
+		_size -= 1;
+	}
 }
