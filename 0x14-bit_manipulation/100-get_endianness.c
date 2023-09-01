03a04a3f280fcb998863b3f1d7cb452f372aedd0
zoraaga <fati.sousse12@gmail.com>
2023-08-31 17:44:23 -0700

    fix betty style

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,7 +3,7 @@
 /**
  * get_endianness - checks if a machine's endianness is little or big.
  * Return: 0 if the machine is big endian,
- * 		1 if it's little.
+ *		1 if it's little.
  */
 
 int get_endianness(void)
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,6 +3,7 @@
 /**
  * set_bit - Sets the value of a bit to 1 at a given index.
  * @n: Pointer to the number whose bit needs to be set.
+ * @index: Index of the bit to be set (starting from 0).
  *
  * Return: 1 if it worked, or -1 if an error occurred.
  */
