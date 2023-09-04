ae296cd7936fdc540f08ba495df25f3c09f34e99
zoraaga <fati.sousse12@gmail.com>
2023-09-04 16:31:14 -0700

    Write a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: file to be read
+ * @letters: number of letters succed to be reading
+ * Return: the actual number of letters it could read and print
+ *		0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *_buffer;
+	ssize_t _file;
+	ssize_t _bytesWritten, _bytesRead;
+
+	_file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	_buffer = malloc(sizeof(char) * letters);
+	_bytesRead = read(_file, _buffer, letters);
+	_bytesWritten = write(STDOUT_FILENO, _buffer, _bytesRead);
+
+	free(_buffer);
+	close(_file);
+	return (_bytesWritten);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,24 @@
+#ifndef main_h
+#define main_h
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <elf.h>
+
+/* Prototypes of used functions */
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void close_file(int fd, const char *filename, const char *operation);
+void copy_file(const char *source, const char *destination);
+void handle_write_error(const char *filename);
+void handle_read_error(const char *filename);
+	
+
+#endif
