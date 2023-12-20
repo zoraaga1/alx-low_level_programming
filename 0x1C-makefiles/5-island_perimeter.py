d476927b4bc5acb76c024cc9a25e6b5da29c9349
zoraaga1 <fati.sousse12@gmail.com>
2023-12-20 21:26:10 +0100

    mandatory tasks

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""island_perimeter module"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid."""
+    perimeter = 0
+
+    if not grid or not grid[0]:
+        return perimeter
+
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
