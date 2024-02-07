724e106b217e8ff281fda6055347fd78dcebd70b
zoraaga1 <fati.sousse12@gmail.com>
2024-02-07 22:43:29 +0100

    mandatory tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,31 +13,31 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0, r = size - 1, m, i;
+	size_t l = 0, r = size - 1, m, i;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (l <= r)
-    {
-        printf("Searching in array: ");
-        for (i = l; i <= r; i++)
-        {
-            printf("%d", array[i]);
-            if (i < r)
-                printf(", ");
-        }
-        printf("\n");
+	while (l <= r)
+	{
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d", array[i]);
+			if (i < r)
+				printf(", ");
+		}
+		printf("\n");
 
-        m = (l + r) / 2;
+		m = (l + r) / 2;
 
-        if (array[m] == value)
-            return (m);
-        else if (array[m] < value)
-            l = m + 1;
-        else
-            r = m - 1;
-    }
+		if (array[m] == value)
+			return (m);
+		else if (array[m] < value)
+			l = m + 1;
+		else
+			r = m - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
